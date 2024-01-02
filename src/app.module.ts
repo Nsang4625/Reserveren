d1bafd4606540ffc1af44037015d5ec57fd559dd
Nguyễn  Sang <sang@Nguyens-MacBook-Pro.local>
2024-01-02 12:28:00 +0700

    adding global exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import * as process from 'process';
 import { BrandModule } from './adapters/brand/brand.module';
+import { APP_FILTER } from '@nestjs/core';
+import { GlobalExceptionFilter } from './infras/filters/global-exception.filter';
 
 @Module({
   imports: [
@@ -24,6 +26,12 @@ import { BrandModule } from './adapters/brand/brand.module';
     BrandModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    }
+  ],
 })
 export class AppModule { }
