23795ee458839cde6595895fbc607f09673cc578
Nguyen Sang <nsang4625@gmail.com>
2024-01-16 16:07:23 +0700

    added fk column for brand_user

--- src/infras/repositories/brand/brands-user.entity.ts
+++ src/infras/repositories/brand/brands-user.entity.ts
@@ -10,6 +10,12 @@ export class BrandUser {
     @Column({ type: 'enum', enum: [ 'owner', 'staff'], default: 'staff'})
     role: string;
 
+    @Column({ name: 'user_id'})
+    userId: string;
+
+    @Column({ name: 'brand_id'})
+    brandId: number;
+
     @ManyToOne(() => UserEntity, user => user.brandUser )
     @JoinColumn({ name: 'user_id'})
     user: UserEntity;
